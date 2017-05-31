68304f38e4228367937388969d671d609444b4a5
Jarred Capellman <jcapellman@hotmail.com>
2017-05-30 21:30:09 -0500

    Removed extraneous pre-refactoring code

--- src/jcHAP.SensorNode/MainPage.xaml.cs
+++ src/jcHAP.SensorNode/MainPage.xaml.cs
@@ -4,30 +4,9 @@ namespace jcHAP.SensorNode
 {
     public sealed partial class MainPage : Page
     {
-        public double GetTemperature()
-        {
-            try
-            {
-                byte[] tempCommand = new byte[1] { 0xE3 };
-                byte[] tempData = new byte[2];
-                sensor.WriteRead(tempCommand, tempData);
-                var rawTempReading = tempData[0] << 8 | tempData[1];
-                var tempRatio = rawTempReading / (float)65536;
-                double temperature = -46.85 + (175.72 * tempRatio);
-                System.Diagnostics.Debug.WriteLine("Temp: " + temperature.ToString());
-                return temperature;
-            }
-            catch (Exception)
-            {
-                return 0.0;
-            }
-        }
-
         public MainPage()
         {
             this.InitializeComponent();
-
-
         }
     }
 }
