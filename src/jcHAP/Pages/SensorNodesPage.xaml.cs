d6f3534a3c3ed01001c8ad831b3c26d844b25273
Jarred Capellman <jcapellman@hotmail.com>
2017-06-03 08:39:51 -0500

    Added: Initial work on the Sensor Node Page

---
+++ src/jcHAP/Pages/SensorNodesPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml.Navigation;
+
+using jcHAP.ViewModels.Pages;
+
+namespace jcHAP.Pages
+{
+    public sealed partial class SensorNodesPage
+    {
+        private SensorNodesViewModel ViewModel => (SensorNodesViewModel) DataContext;
+
+        public SensorNodesPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = new SensorNodesViewModel();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await ViewModel.LoadSensorNodes();
+
+            OnNavigatedTo(e);
+        }
+    }
+}
---
+++ src/jcHAP/ViewModels/Pages/SensorNodesViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+using jcHAP.Library.DAL.SQLite.Tables;
+using jcHAP.Library.Managers;
+
+namespace jcHAP.ViewModels.Pages
+{
+    public class SensorNodesViewModel : BasePageViewModel
+    {
+        private ObservableCollection<SensorNodes> _sensorNodes;
+
+        public ObservableCollection<SensorNodes> SensorNodes
+        {
+            get { return _sensorNodes; }
+            set { _sensorNodes = value; OnPropertyChanged(); }
+        }
+
+        public async Task<bool> LoadSensorNodes()
+        {
+            var snManager = new SensorNodeManager();
+
+            var nodes = await snManager.GetSensorNodes();
+            
+            SensorNodes = new ObservableCollection<SensorNodes>(nodes);
+
+            return true;
+        }
+    }
+}
