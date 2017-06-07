5c5e975c0c99898b5454011fe369ad7d6f8afd19
Jarred Capellman <jcapellman@hotmail.com>
2017-06-06 20:36:58 -0500

    Added: Wireless Network discovery logic

--- src/jcHAP/ViewModels/Pages/SettingsViewModel.cs
+++ src/jcHAP/ViewModels/Pages/SettingsViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
+
+using Windows.Devices.WiFi;
 
 using jcHAP.Library.Managers;
 using jcHAP.Library.Objects.Settings;
@@ -17,6 +21,14 @@ namespace jcHAP.ViewModels.Pages
             set { _settings = value; OnPropertyChanged(); }
         }
 
+        private ObservableCollection<WiFiAvailableNetwork> _wirelessNetworks;
+
+        public ObservableCollection<WiFiAvailableNetwork> WirelessNetworks
+        {
+            get => _wirelessNetworks;
+            set { _wirelessNetworks = value; OnPropertyChanged(); }
+        }
+
         public async void LoadData()
         {
             Settings = new ObservableCollection<SettingsListingResponseItem>();
@@ -31,6 +43,38 @@ namespace jcHAP.ViewModels.Pages
             var settingsManager = new SettingsManager();
 
             return await settingsManager.SaveSettingsAsync(new List<SettingsListingResponseItem>(Settings));
+        }
+
+        private async void LoadWirelessNetworks()
+        {
+            var access = await WiFiAdapter.RequestAccessAsync();
+
+            // No Access - short circuit
+            if (access != WiFiAccessStatus.Allowed)
+            {
+                return;
+            }
+
+            var result = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(WiFiAdapter.GetDeviceSelector());
+
+            // No WiFi Card - short circuit
+            if (!result.Any())
+            {
+                return;
+            }
+
+            var wiFiAdapter = await WiFiAdapter.FromIdAsync(result[0].Id);
+
+            wiFiAdapter.AvailableNetworksChanged += wifiAdapter_AvailableNetworksChanged;
+
+            await wiFiAdapter.ScanAsync();
+        }
+
+        private void wifiAdapter_AvailableNetworksChanged(WiFiAdapter sender, object args)
+        {
+            var networks = sender.NetworkReport.AvailableNetworks.ToList();
+
+            WirelessNetworks = new ObservableCollection<WiFiAvailableNetwork>(networks);
         }
     }
 }
